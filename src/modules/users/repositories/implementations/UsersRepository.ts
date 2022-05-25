0be09192f930e9de6204a703f9d0afcd376d1113
Rafael Medeiros Ribeiro <rafaelmedrib@gmail.com>
2022-05-25 09:27:59 -0300

    :heavy_check_mark: 4 passed / 2 failed

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -14,17 +14,26 @@ export class UsersRepository implements IUsersRepository {
   async findUserWithGamesById({
     user_id,
   }: IFindUserWithGamesDTO): Promise<User> {
-    // Complete usando ORM
+    const user = await this.repository.findOne({ id: user_id });
+    
+    if(!user){
+      throw new Error("User not found");
+    }
+
+    return user
   }
 
   async findAllUsersOrderedByFirstName(): Promise<User[]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query(`
+    SELECT * FROM users ORDER BY first_name
+    `);
   }
 
   async findUserByFullName({
     first_name,
     last_name,
   }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    return this.repository.query(); // Complete usando raw query
+    const query = `SELECT * FROM users WHERE LOWER(first_name) = '${first_name.toLowerCase()}' AND LOWER(last_name) = '${last_name.toLowerCase()}'`
+    return this.repository.query(query); // Complete usando raw query
   }
 }
